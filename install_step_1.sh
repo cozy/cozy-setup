2270a3fb0e4cd81a3cfb9d3912f0e8045ea178d7
Frank Rousseau <frankrousseau@sommerless.(none)>
2012-04-18 19:24:08 +0200

    upgrade to node 0.6.15

--- install_step_1.sh
+++ install_step_1.sh
@@ -6,15 +6,15 @@ sudo apt-get install python openssl libssl-dev pkg-config
 
 # Install node js
 apt-get install g++ make
-wget http://nodejs.org/dist/v0.6.12/node-v0.6.12.tar.gz
-tar -xvzf node-v0.6.12.tar.gz 
-cd node-v0.6.12
+wget http://nodejs.org/dist/v0.6.15/node-v0.6.15.tar.gz
+tar -xvzf node-v0.6.15.tar.gz 
+cd node-v0.6.15
 ./configure
 make
 sudo make install
 cd ..
-rm node-v0.6.12.tar.gz
-rm -rf node-v0.6.12
+rm node-v0.6.15.tar.gz
+rm -rf node-v0.6.15
 
 # Install Upstart 
 sudo apt-get install upstart

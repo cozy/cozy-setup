1e5326dd79f9810d2f762d7dfb42e0b6e79fe56d
Frank Rousseau <frank@mycozycloud.com>
2012-05-22 14:45:00 +0200

    add description comments to install script for devs

--- install_dev.sh
+++ install_dev.sh
@@ -1,9 +1,13 @@
+# Script that help developers which use Ubuntu to have a complete environment 
+# for working on Cozy tools.
+
+# Update system
 sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install python openssl libssl-dev pkg-config
 sudo apt-get install g++ make
 
-# install node
+# install Node JS
 wget http://nodejs.org/dist/v0.6.15/node-v0.6.15.tar.gz
 tar -xvzf node-v0.6.15.tar.gz 
 cd node-v0.6.15

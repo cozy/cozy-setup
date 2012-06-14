4c1524d837f4a64cfe19fa32941c62f2001d7813
tosh <tosh@tosh.(none)>
2012-06-14 14:31:36 +0200

    add du paas.conf et maj fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,8 +3,8 @@ from fabric.api import run
 def install():
     update()
     install_tools()
+    #restart...??
     install_nodejs()
-    #TODO reboot sinon ca merde
     install_mongodb()
 
 # Maintain Up to Date the system
@@ -14,7 +14,7 @@ def update():
 
 # Tools install
 def install_tools():
-    run('sudo apt-get install python openssl libssl-dev pkg-config g++ ')
+    run('sudo apt-get install python openssl libssl-dev pkg-config g++ git')
     run('sudo apt-get install upstart')
 
 # Installing Node 0.18
@@ -31,6 +31,7 @@ def install_mongodb():
     run('sudo apt-get update')
     run('sudo apt-get install mongodb-10gen')
 
+# Installing and Auto-starting Redis
 def install_redis():
     run('wget http://redis.googlecode.com/files/redis-2.4.14.tar.gz')
     run('tar xvzf redis-2.4.14.tar.gz')
@@ -40,6 +41,30 @@ def install_redis():
     run('sudo mkdir /etc/redis')
     run('sudo mkdir /var/redis ; sudo mkdir /var/redis/6379')
     run('sudo cp redis-2.4.14/utils/redis_init_script /etc/init.d/redis_6379')
-    ##todo importer la conf sudo cp 6379.conf /etc/redis/
-    #run('sudo update-rc.d redis_6379 defaults')
+    run('sudo cp /vagrant/6379.conf /etc/redis/')
+    #todo importer la conf sudo cp cozy-setup/6379.conf /etc/redis/
+    run('sudo update-rc.d redis_6379 defaults')
     run('sudo /etc/init.d/redis_6379 start')
+
+# Preparing Cozy
+def install_preinstall():
+    run('sudo apt-get install postfix')
+    run('sudo npm install -g coffee-script')
+    run('sudo npm install -g haibu')
+    #TODO vagrant a modofier par le repo
+    run('sudo cp /vagrant/paas.conf /etc/init/')
+    run('sudo service paas start')
+
+    # setup certs
+    run('sudo openssl genrsa -out ./server.key 1024')
+    run('sudo openssl req -new -x509 -days 3650 -key ./server.key -out ./server.crt')
+    run('sudo chmod 640 server.key')
+    run('sudo chown root:ssl-cert server.key')
+
+def install_cozy():
+    run('sudo git clone https://github.com/mycozycloud/cozy-setup.git')
+    run('cd cozy-setup ; sudo npm install eyes haibu@0.8.2')
+    run('sudo mkdir /usr/local/lib/node_modules/haibu/local/')
+    run('sudo coffee home.coffee')
+    run('sudo coffee notes.coffee')
+    run('sudo coffee proxy.coffee')

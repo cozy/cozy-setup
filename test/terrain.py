7b59dba642eef20f0d71800b860520a56bc20571
Frank Rousseau <frank@mycozycloud.com>
2012-06-20 22:28:42 +0200

    test preparation

--- test/terrain.py
+++ test/terrain.py
@@ -1,10 +1,1 @@
-from splinter.browser import Browser
-from lettuce import before, after, world
-
-@before.all
-def initial_setup(server):    
-    world.browser = Browser('webdriver.firefox')
-
-@after.all
-def teardown_browser(total):
-    world.browser.quit()
+from salad.terrains.everything import *

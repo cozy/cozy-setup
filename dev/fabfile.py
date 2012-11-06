86831d2e0397b4cdfc7650030a3d0745a97d9e9c
Frank Rousseau <frank.rousseau@free.fr>
2012-11-06 21:16:44 +0100

    add redis task

--- dev/fabfile.py
+++ dev/fabfile.py
@@ -1,3 +1,5 @@
+import os
+
 from fabric.api import local, lcd, task
 from fabric.colors import green
 
@@ -22,11 +24,40 @@ def cozydo(cmd):
     """
     sudo(cmd, "cozy")
 
+def mkdir(dirname, user):
+    """
+    Create dir if it does not exist and give ownership to given user.
+    """
+    if not os.path.exists(dirname):
+        sudo("mkdir -p %s" % dirname)
+    sudo("chown %s:%s %s" % (user, user, dirname))
+
 def install_packages(packages):
     """
     Install given packages through apt-get.
     """
     local("sudo apt-get install %s" % ' '.join(x for x in packages))
+
+def build_config_file(filename, params):
+    """
+    Build config with given params while following this scheme:
+    
+        key1=value1
+        key2=value2
+        ...
+        keyn=valuen
+    """
+    lines = []
+    lines.append('[program:%(name)s]' % locals())
+    for key, value in sorted(params.items()):
+        lines.append("%s=%s" % (key, value))
+
+    filename = '/etc/supervisor/conf.d/%(name)s.conf' % locals()
+    file_content = '\n'.join(lines)
+    with lcd("/tmp"):
+        local('echo "%s" > conf.tmp' % file_content)
+        local("sudo cp conf.tmp %s" % filename)
+        local("rm conf.tmp")
 
 def update_supervisor():
     """
@@ -38,24 +69,13 @@ def add_process(name, **kwargs):
     """
     Add configuration file to supervisor for given process.
     """
-    # Set default parameters
     params = {}
     params.update(kwargs)
     params.setdefault('autorestart', 'true')
     params.setdefault('redirect_stderr', 'true')
-
-    # Build config file from parameters
-    lines = []
-    lines.append('[program:%(name)s]' % locals())
-    for key, value in sorted(params.items()):
-        lines.append("%s=%s" % (key, value))
-
-    # Upload config file
+    
     filename = '/etc/supervisor/conf.d/%(name)s.conf' % locals()
-    file_content = '\n'.join(lines)
-    local('echo "%s" > conf.tmp' % file_content)
-    local("sudo cp conf.tmp %s" % filename)
-    local("rm conf.tmp")
+    build_config_file(filename, params)
     update_supervisor()
      
 
@@ -119,7 +139,7 @@ def install_couchdb():
 
     sudo('adduser --system --home /usr/local/var/lib/couchdb '+
         '--no-create-home --shell /bin/bash --group --gecos '+
-        '"CouchDB_Administrator" couchdb')
+        '"couchdb" couchdb')
     sudo('chown -R couchdb:couchdb /usr/local/etc/couchdb')
     sudo('chown -R couchdb:couchdb /usr/local/var/lib/couchdb')
     sudo('chown -R couchdb:couchdb /usr/local/var/log/couchdb')
@@ -142,7 +162,57 @@ def install_redis():
     """
     Installing and Auto-starting Redis 2.4.14. Use supervisord to daemonize it.
     """
-    pass
+    BINARIES = [
+            'redis-benchmark',
+        'redis-check-aof',
+        'redis-check-dump',
+        'redis-cli',
+        'redis-server',
+    ]
+    name = "cozy"
+    dest_dir = '/opt/redis-2.4.14'
+
+    sudo('adduser --system --home /usr/local/var/lib/redis '+
+        '--no-create-home --shell /bin/bash --group --gecos '+
+        '"redis" redis')
+    
+    with lcd('/tmp'):
+        local('http://redis.googlecode.com/files/redis-2.4.14.tar.gz')
+        local('tar xzf redis-2.4.14.tar.gz')
+
+    with lcd('/tmp/redis-2.4.14'):
+        local('make')
+
+        for filename in BINARIES:
+            sudo('cp -pf src/%(filename)s %(dest_dir)s/' % locals())
+            sudo('chown redis: %(dest_dir)s/%(filename)s' % locals())
+
+    mkdir('/etc/redis', 'redis')
+    mkdir('/var/db/redis', 'redis')
+    mkdir('/var/log/redis', 'redis')
+    mkdir('/var/run/redis', 'redis')
+
+    params = {}
+    params.setdefault('bind', '127.0.0.1')
+    params.setdefault('port', '6379')
+    params.setdefault('logfile', '/var/log/redis/redis-%(name)s.log' % locals())
+    params.setdefault('loglevel', 'verbose')
+    params.setdefault('dbfilename', '/var/db/redis/redis-%(name)s-dump.rdb' % locals())
+    params.setdefault('save', ['900 1', '300 10', '60 10000'])
+    build_config_file("/opt/redis-2.4.14/redis-server", params)
+
+@task
+def set_data_redis_process():
+    """
+    Daemonize Data Indexer with supervisor.
+    """
+    redis_bin = '/opt/redis-2.4.14/redis-server'
+    redis_config = '/etc/redis/cozy.conf'
+
+    add_process('redis', user='redis', 
+        command='%s %s' % (redis_bin, redis_config),
+        directory='/var/run/redis',
+        autostart='true')
 
 @task
 def create_cozy_user():

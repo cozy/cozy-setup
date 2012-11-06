a91e60346cbf9a03ebc711fd75b6d1615fc5eab9
Frank Rousseau <frank.rousseau@free.fr>
2012-11-06 21:28:10 +0100

    put right file name for redis conf

--- dev/fabfile.py
+++ dev/fabfile.py
@@ -200,7 +200,7 @@ def install_redis():
     params.setdefault('loglevel', 'verbose')
     params.setdefault('dbfilename', '/var/db/redis/redis-%(name)s-dump.rdb' % locals())
     params.setdefault('save', ['900 1', '300 10', '60 10000'])
-    build_config_file("/opt/redis-2.4.14/redis-server", params)
+    build_config_file("/etc/redis/%(name)s.conf", params)
 
 @task
 def set_data_redis_process():

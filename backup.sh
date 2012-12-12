0780f0140094e7f5e21e4526a66871f7ba0a6f1c
root <root@cozy-lucas-toulouse-1.(none)>
2012-12-12 13:43:49 +0000

    Script backup with targeturl ok

--- backup.sh
+++ backup.sh
@@ -2,4 +2,20 @@
 #Usage
 #./backup.sh urltarget urlbackup
 #
-curl -X POST http://127.0.0.1:5984/_replicate -d {"source"="$1","target"="$2"}
+
+if [ $# = 1 ]
+    then
+	backupAnswer=$(curl -X POST -H "Content-Type: application/json" -d '{"source":"cozy","target":"'$1'","verify_ssl_certificates":false}' http://127.0.0.1:5984/_replicate) 
+	if [[ "$backupAnswer" == *"\"ok\":true"* ]]
+	    then
+		echo "Backup OK"
+		exit 0
+	    else 
+		echo "Backup FAILED"
+		exit 1
+	fi
+	
+    else
+	echo "Usage: $0 targetUrl"
+	exit 1
+fi

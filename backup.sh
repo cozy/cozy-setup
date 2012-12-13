e79f100fbd6d32e69785ec929ed9c45cb8bbdc8a
LucsT <lucas.toulouse@gmail.com>
2012-12-13 11:16:57 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Usage
-#./backup.sh urltarget urlbackup
+#./backup.sh urltarget 
 #
 
 if [ $# = 1 ]
@@ -8,10 +8,10 @@ if [ $# = 1 ]
 	backupAnswer=$(curl -X POST -H "Content-Type: application/json" -d '{"source":"cozy","target":"'$1'","verify_ssl_certificates":false}' http://127.0.0.1:5984/_replicate) 
 	if [[ "$backupAnswer" == *"\"ok\":true"* ]]
 	    then
-		echo "Backup OK"
+		#Backup Ok
 		exit 0
 	    else 
-		echo "Backup FAILED"
+		#Backup FAILED
 		exit 1
 	fi
 	

8f6e2aeed690b77fb66a2ec5284eaa9542870d94
Joseph Silvestre <joseph.silvestre38@gmail.com>
2013-02-20 12:23:26 +0100

    Added: a quickfix to start redis

--- dev/init-cozycloud.sh
+++ dev/init-cozycloud.sh
@@ -4,6 +4,28 @@
 # The monitor is told to start the data-system, the home and the proxy app.
 # This is the last step needed to be able to develop a cozy app with the virtual machine.
 
+
+
+#####
+#
+# Temporary section. This allows us to quickfix the bug that deletes redis folder
+# each time the VM is stopped.
+#
+#####
+
+redis_folder="/var/run/redis"
+sudo mkdir -p $redis_folder
+sudo chown redis:redis $redis_folder
+sudo supervisorctl restart redis_cozy
+
+
+
+#####
+#
+# Starts needed Cozy Applications
+#
+#####
+
 coffee="/usr/local/bin/coffee"
 target="/home/cozy/cozy-setup/monitor.coffee"
 

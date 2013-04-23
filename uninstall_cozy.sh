fafd11f3c0a456903e85a62fa8ac2f11a82fcc6d
Frank Rousseau <frank.rousseau@free.fr>
2013-04-23 18:57:41 +0200

    debug install line

--- uninstall_cozy.sh
+++ uninstall_cozy.sh
@@ -3,4 +3,4 @@
 apt-get install python python-pip
 sudo pip install fabric==1.6.0 fabtools==0.13.0
 wget https://raw.github.com/mycozycloud/cozy-setup/master/fabfile.py
-fab -H $1 uninstall
+fab -H $HOST uninstall

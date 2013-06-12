aa61ca8fcf62462594b324044dfa0c7aed23f01e
Frank Rousseau <frank.rousseau@free.fr>
2013-06-12 23:39:31 +0200

    add python-dev to required dependencies to run cozy install script

--- install_cozy.sh
+++ install_cozy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo apt-get install python python-pip
+sudo apt-get install python python-pip python-dev
 sudo pip install fabric==1.6.0 fabtools==0.13.0
 wget https://raw.github.com/mycozycloud/cozy-setup/master/fabfile.py
 fab -H $HOST install

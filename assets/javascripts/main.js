f8a23035ea6860a53c0c39f270c4bd875c52f818
Frank Rousseau <frank.rousseau@cozycloud.cc>
2013-09-27 18:54:18 +0200

    new website

---
+++ assets/javascripts/main.js
@@ -0,0 +1,14 @@
+(function() {
+  $(document).ready(function() {
+    /*
+    $('nav li').click ->
+        $('nav li.selected').find('ul').hide()
+        $('nav li.selected').removeClass 'selected'
+        $(this).addClass 'selected'
+    
+        $(this).find('ul').show()
+    */
+
+  });
+
+}).call(this);
--- uninstall_cozy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-apt-get install python python-pip
-sudo pip install fabric==1.6.0 fabtools==0.13.0
-wget https://raw.github.com/mycozycloud/cozy-setup/master/fabfile.py
-fab -H $HOST uninstall

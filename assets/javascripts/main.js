4e89affe09ec2dbd7cb728fd6a128f2427f4e1a5
Frank Rousseau <frank.rousseau@cozycloud.cc>
2013-11-11 10:04:40 +0100

    rebuild everything

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,10 +1,14 @@
-$(document).ready ->
-
-    ###
+(function() {
+  $(document).ready(function() {
+    /*
     $('nav li').click ->
         $('nav li.selected').find('ul').hide()
         $('nav li.selected').removeClass 'selected'
         $(this).addClass 'selected'
-
+    
         $(this).find('ul').show()
-    ###
+    */
+
+  });
+
+}).call(this);

f5382ac47c361d5244dab1c5e446d35e65b8c3bb
Frank Rousseau <frank.rousseau@cozycloud.cc>
2013-11-11 09:54:43 +0100

    last doc modifications

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,14 +1,10 @@
-(function() {
-  $(document).ready(function() {
-    /*
+$(document).ready ->
+
+    ###
     $('nav li').click ->
         $('nav li.selected').find('ul').hide()
         $('nav li.selected').removeClass 'selected'
         $(this).addClass 'selected'
-    
+
         $(this).find('ul').show()
-    */
-
-  });
-
-}).call(this);
+    ###

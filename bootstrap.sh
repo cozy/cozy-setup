cff0c6a79efa4295c06a2f803d6e32eef341ef6f
Arnaud Vazard <vaz-git@gmx.com>
2014-06-17 23:04:25 +0200

    Updated ez_setup URL
    Changed the URL to download ez_setup according to the the new URL (see
    https://bitbucket.org/pypa/setuptools/issue/192/any-attempt-to-download-ez_setuppy-fails
    for details)

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,9 +3,8 @@
 requirements="fabric fabtools"
 
 if [[ "$VIRTUAL_ENV" == "" ]]; then
-    curl -o - https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O - | python
+    curl -o - https://bootstrap.pypa.io/ez_setup.py -O - | python
     curl -o - https://raw.githubusercontent.com/pypa/pip/master/contrib/get-pip.py | python -
-
 fi
 
 pip install -U $requirements
